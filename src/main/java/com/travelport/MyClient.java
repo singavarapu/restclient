509a2959507ce8d52285d7934f68b1d357ca26ab
Krupa Singavarapu <krupa.singavarapu@gmail.com>
2018-11-14 11:25:57 -0700

    switch to original

--- src/main/java/com/travelport/MyClient.java
+++ src/main/java/com/travelport/MyClient.java
@@ -5,8 +5,8 @@ import org.springframework.web.client.RestTemplate;
 public class MyClient {
     public static void clientGet()
     {
-        final String uri = "https://is-echo-monitor-a.adc-dv-gf.travelport.com/management";
-//        final String uri = "https://health-app-health-app.ocp-a.zu2.nonprod.travelport.io/actuator";
+ //       final String uri = "https://is-echo-monitor-a.adc-dv-gf.travelport.com/management";
+       final String uri = "https://health-app-health-app.ocp-a.zu2.nonprod.travelport.io/actuator";
 //        final String uri = "http://localhost:8080/actuator";
 
         RestTemplate restTemplate = new RestTemplate();
